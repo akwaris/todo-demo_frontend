7bb0c8a850cc2e4363434458366a9c9ad389ea94
Waris Akbari <akb220516@spengergasse.at>
2023-09-30 17:26:36 +0200

    setup frontend tooling

--- apps/info-pilot/src/app/app.component.spec.ts
+++ apps/info-pilot/src/app/app.component.spec.ts
@@ -4,25 +4,25 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent, NxWelcomeComponent],
-    }).compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
+			declarations: [AppComponent, NxWelcomeComponent],
+		}).compileComponents();
+	});
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome info-pilot'
-    );
-  });
+	it('should render title', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.nativeElement as HTMLElement;
+		expect(compiled.querySelector('h1')?.textContent).toContain(
+			'Welcome info-pilot'
+		);
+	});
 
-  it(`should have as title 'info-pilot'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('info-pilot');
-  });
+	it(`should have as title 'info-pilot'`, () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app.title).toEqual('info-pilot');
+	});
 });
